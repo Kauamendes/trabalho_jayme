0698e37d52868cdb55d38da4b17d8b62e5ffb8be
kaua.mendes@attornatus.com.br <kaua.mendes@attornatus.com.br>
2023-06-27 09:01:07 -0300

    Finalizando backend

---
+++ musicas.js
@@ -0,0 +1,47 @@
+export default [
+    {
+      src: "./music/[YT2mp3.info] - Costa Gold -  N.A.D.A.B.O.M PT 3 (320kbps).mp3",
+      img: "./images/nadabom3.jpeg",
+      name: "Costa Gold - N.A.D.A.B.O.M PT 3",
+    },
+    {
+      src: "./music/Numb (Official Music Video) [4K UPGRADE] – Linkin Park.mp3",
+      img: "./images/numb.jpeg",
+      name: "Numb - Linkin Park",
+    },
+    {
+      src: "./music/SnapInsta.io - Avicii - The Nights (128 kbps).mp3",
+      img: "./images/the_nights.jpg",
+      name: "The Nights - Avicii",
+    },
+    {
+      src: "./music/SnapInsta.io - Edi Rock - That's My Way ft. Seu Jorge [Video Oficial] (192 kbps).mp3",
+      img: "./images/thats_my_way.jpg",
+      name: "That's My Way ft. Seu Jorge - Edi Rock",
+    },
+    {
+      src: "./music/SnapInsta.io - Imprevisto - Yago Oproprio ft. Rô Rosa (Clipe Oficial) (128 kbps).mp3",
+      img: "./images/imprevisto.jpg",
+      name: "Imprevisto - Yago Oproprio ft. Rô Rosa  - Yago Oproprio",
+    },
+    {
+      src: "./music/SnapInsta.io - Lagum, L7NNON, Mart'nália - EITA MENINA (128 kbps).mp3",
+      img: "./images/eita_menina.jpg",
+      name: "EITA MENINA - Lagum, L7NNON, Mart'nália",
+    },
+    {
+        src: "./music/SnapInsta.io - Lil Nas X - Old Town Road (Official Movie) ft. Billy Ray Cyrus (128 kbps).mp3",
+        img: "./images/old_town_road.jpg",
+        name: "Old Town Road - Lil Nas X",
+      },
+      {
+        src: "./music/SnapInsta.io - MC CABELINHO - X1 (prod. DALLASS) (256 kbps).mp3",
+        img: "./images/x1.jpg",
+        name: "X1 (prod. DALLASS) - MC CABELINHO",
+      },
+      {
+        src: "./music/SnapInsta.io - Travis Scott - goosebumps ft. Kendrick Lamar (128 kbps).mp3",
+        img: "./images/goosebumps.jpg",
+        name: "goosebumps ft. Kendrick Lamar - Travis Scott",
+      },
+  ];
--- script.js
+++ script.js
@@ -1,9 +1,73 @@
-let musica = document.querySelector('audio');
+const reprodutor = document.querySelector("#reprodutor");
+const imagemMusica = document.querySelector("#imagemMusica");
+const nomeMusica = document.querySelector("#nomeMusica");
+const botaoPlayPausa = document.querySelector("#botaoPlayPausa");
+const botaoAnterior = document.querySelector("#botaoAnterior");
+const botaoProximo = document.querySelector("#botaoProximo");
+const tempoAtual = document.querySelector("#tempoAtual");
+const duracaoTotal = document.querySelector("#duracaoTotal");
+const barraProgresso = document.querySelector(".barra-progresso");
+const progresso = document.querySelector(".progresso");
 
-document.querySelector('.botao-play').addEventListener('click', tocarMusica());
+import musicas from "./musicas.js";
 
-function tocarMusica() {
-    musica.play();
-    document.querySelector('.botao-pause').style.display = 'block';
-    document.querySelector('.botao-play').style.display = 'none';
+const textoBotaoPlay = "<i class='bx bx-caret-right'></i>";
+const textoBotaoPause = "<i class='bx bx-pause'></i>";
+
+let indice = 0;
+
+botaoAnterior.onclick = () => tocarProximaOuAnterior("prev");
+botaoProximo.onclick = () => tocarProximaOuAnterior();
+
+botaoPlayPausa.onclick = () => {
+  if (reprodutor.paused) {
+    reprodutor.play();
+    botaoPlayPausa.innerHTML = textoBotaoPause;
+  } else {
+    reprodutor.pause();
+    botaoPlayPausa.innerHTML = textoBotaoPlay;
+  }
+};
+
+function atualizarTempo() {
+    const minutosAtuais = Math.floor(reprodutor.currentTime / 60);
+    const segundosAtuais = Math.floor(reprodutor.currentTime % 60);
+    tempoAtual.textContent = `${minutosAtuais}:${formatarZero(segundosAtuais)}`;
+  
+    const duracaoFormatada = isNaN(reprodutor.duration) ? 0 : reprodutor.duration;
+    const minutosDuracao = Math.floor(duracaoFormatada / 60);
+    const segundosDuracao = Math.floor(duracaoFormatada % 60);
+    duracaoTotal.textContent = `${minutosDuracao}:${formatarZero(segundosDuracao)}`;
+  
+    const larguraProgresso = duracaoFormatada ? (reprodutor.currentTime / duracaoFormatada) * 100 : 0;
+  
+    progresso.style.width = `${larguraProgresso}%`;
 }
+
+barraProgresso.onclick = (e) => {
+  const novoTempo = (e.offsetX / barraProgresso.offsetWidth) * reprodutor.duration;
+  reprodutor.currentTime = novoTempo;
+};
+
+function tocarProximaOuAnterior(tipo) {
+    if (tipo === "proxima") {
+      indice = (indice + 1) % musicas.length;
+    } else if (tipo === "anterior") {
+      indice = (indice - 1 + musicas.length) % musicas.length;
+    } else if (tipo === "iniciar") {
+      indice = 0;
+    }
+
+  reprodutor.src = musicas[indice].src;
+  nomeMusica.innerHTML = musicas[indice].name;
+  imagemMusica.innerHTML = musicas[indice].img;
+  if (tipo !== "iniciar") playPause();
+
+  atualizarTempo();
+}
+
+function formatarZero(n) {
+  return n < 10 ? "0" + n : n;
+}
+
+tocarProximaOuAnterior("iniciar");
